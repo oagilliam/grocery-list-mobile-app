2dcc3e9a59100cfcbc49ca1790b41c69641cbacf
Olivia Gilliam <oagilliam@outlook.com>
2023-09-04 20:29:47 -0400

    function added

--- index.js
+++ index.js
@@ -2,21 +2,29 @@ import{ initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase
 import{ getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://grocery-store-app-c4216-default-rtdb.firebaseio.com/";
+    databaseURL: "https://grocery-store-app-c4216-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const itemsInDB = ref(database, "items");
 
-let addBtn = document.getElementById("add-button");
-let inputFld = document.getElementById("input-field");
-let shoppingLst = document.getElementById("shopping-list");
+const addBtn = document.getElementById("add-button");
+const inputFld = document.getElementById("input-field");
+const shoppingLst = document.getElementById("shopping-list");
+const clearInput= ''
 
 addBtn.addEventListener("click", function(){
     let inputValue = inputFld.value;
     push(itemsInDB, inputValue);
-
-    //Adds items to queue
-    shoppingLst.innerHTML += `<li>${inputValue}</li>`;
+    clearInputFld()
+    appendItemToShoppingList(inputValue)
 })
+
+function clearInputFld() {
+    inputFld.value = clearInput
+}
+
+function appendItemToShoppingList(itemValue) {
+    shoppingLst.innerHTML += `<li>${itemValue}</li>`
+}
