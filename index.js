a43dae1d5cbe413263430923b773b0b2facde3d3
Olivia Gilliam <oagilliam@outlook.com>
2023-09-04 20:10:30 -0400

    css

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ import{ initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase
 import{ getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://grocery-store-app-c4216-default-rtdb.firebaseio.com/"
+    databaseURL: "https://grocery-store-app-c4216-default-rtdb.firebaseio.com/";
 }
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-const itemsInDB = ref(database, "items")
+const itemsInDB = ref(database, "items");
 
 let addBtn = document.getElementById("add-button");
 let inputFld = document.getElementById("input-field");
-
+let shoppingLst = document.getElementById("shopping-list");
 
 addBtn.addEventListener("click", function(){
-    let inputValue = inputFld.value
-    push(itemsInDB, inputValue)
+    let inputValue = inputFld.value;
+    push(itemsInDB, inputValue);
+
+    //Adds items to queue
+    shoppingLst.innerHTML += `<li>${inputValue}</li>`;
 })
